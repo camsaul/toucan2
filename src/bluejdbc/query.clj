e77cd3470d75fe53bfcbdfd32b0e83a02d6f28b5
Cam Saul <github@camsaul.com>
2020-04-23 16:52:00 -0700

    conn -> connectable

--- src/bluejdbc/query.clj
+++ src/bluejdbc/query.clj
@@ -16,7 +16,7 @@
      (stmt/proxy-prepared-statement x y)
      (reducible-query x y nil)))
 
-  ([conn query-or-stmt options]
+  ([connectable query-or-stmt options]
    (reify
      pretty/PrettyPrintable
      (pretty [_]
@@ -24,7 +24,7 @@
 
      clojure.lang.IReduceInit
      (reduce [_ rf init]
-       (conn/with-connection [conn conn options]
+       (conn/with-connection [conn connectable options]
          (stmt/with-prepared-statement [stmt conn query-or-stmt options]
            (reduce rf init stmt))))
 
@@ -40,7 +40,7 @@
     *  [sql & parameters] or [honeysql-form & parameters]
     *  A `PreparedStatement`
     *  Anything else that you've added that implements `bluejdbc.statement.CreatePreparedStatement`"
-  {:arglists '([stmt] [stmt options] [conn query-or-stmt] [conn query-or-stmt options])}
+  {:arglists '([stmt] [stmt options] [connectable query-or-stmt] [connectable query-or-stmt options])}
   [& args]
   (reduce conj [] (apply reducible-query args)))
 
@@ -49,7 +49,7 @@
 
 (defn query-one
   "Like `query`, but only returns the first row. Other rows are not fetched."
-  {:arglists '([stmt] [stmt options] [conn query-or-stmt] [conn query-or-stmt options])}
+  {:arglists '([stmt] [stmt options] [connectable query-or-stmt] [connectable query-or-stmt options])}
   ([stmt]
    (reduce-first (stmt/proxy-prepared-statement stmt {:stmt/max-rows 1})))
 
@@ -58,15 +58,15 @@
      (reduce-first (stmt/proxy-prepared-statement x (assoc y :stmt/max-rows 1)))
      (query-one x y nil)))
 
-  ([conn query-or-stmt options]
-   (reduce-first (reducible-query conn query-or-stmt (assoc options :stmt/max-rows 1)))))
+  ([connectable query-or-stmt options]
+   (reduce-first (reducible-query connectable query-or-stmt (assoc options :stmt/max-rows 1)))))
 
 (defn execute!
   "Execute a SQL *statement*, which can be SQL/HoneySQL/a `PreparedStatement`. The content of `query-or-stmt` itself
   must be either a DML statement such as `INSERT`, `UPDATE`, or `DELETE`, or a SQL statement that returns nothing,
   such as a DDL statement such as `CREATE TABLE`. Returns either the number of rows modified (for DML statements) or
   0 (for statements that return nothing)."
-  {:arglists '(^Integer [stmt] ^Integer [stmt options] ^Integer [conn query-or-stmt] ^Integer [conn query-or-stmt options])}
+  {:arglists '(^Integer [stmt] ^Integer [stmt options] ^Integer [connectable query-or-stmt] ^Integer [connectable query-or-stmt options])}
   ([stmt]
    (execute! stmt nil))
 
@@ -76,8 +76,8 @@
        (.executeUpdate stmt))
      (execute! x y nil)))
 
-  ([conn query-or-stmt options]
-   (conn/with-connection [conn conn options]
+  ([connectable query-or-stmt options]
+   (conn/with-connection [conn connectable options]
      (stmt/with-prepared-statement [stmt conn query-or-stmt]
        (.executeUpdate stmt)))))
 
@@ -85,26 +85,26 @@
   "Convenience for inserting rows. `rows` can either be maps or vectors. You should supply `columns` if rows are not
   maps.
 
-    (insert! conn :user [{:id 1, :name \"Cam\"} {:id 2, :name \"Sam\"}])
+    (insert! connectable :user [{:id 1, :name \"Cam\"} {:id 2, :name \"Sam\"}])
 
-    (insert! conn :user [:id :name] [[1 \"Cam\"] [2 \"Sam\"]]
+    (insert! connectable :user [:id :name] [[1 \"Cam\"] [2 \"Sam\"]]
 
   Returns number of rows inserted."
-  {:arglists '(^Integer [conn table-name rows]
-               ^Integer [conn table-name columns rows]
-               ^Integer [conn table-name rows options]
-               ^Integer [conn table-name columns rows options])}
-  ([conn table-name rows]
-   (insert! conn table-name nil rows nil))
+  {:arglists '(^Integer [connectable table-name rows]
+               ^Integer [connectable table-name columns rows]
+               ^Integer [connectable table-name rows options]
+               ^Integer [connectable table-name columns rows options])}
+  ([connectable table-name rows]
+   (insert! connectable table-name nil rows nil))
 
-  ([conn table-name x y]
+  ([connectable table-name x y]
    (if (map? y)
-     (insert! conn table-name nil x   y)
-     (insert! conn table-name x   y nil)))
+     (insert! connectable table-name nil x   y)
+     (insert! connectable table-name x   y nil)))
 
-  ([conn table-name columns rows options]
+  ([connectable table-name columns rows options]
    (let [honeysql-form (merge {:insert-into table-name
                                :values      rows}
                               (when (seq columns)
                                 {:columns columns}))]
-     (execute! conn honeysql-form options))))
+     (execute! connectable honeysql-form options))))
