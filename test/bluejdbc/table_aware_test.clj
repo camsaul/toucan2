84b4f5f6224bb193673f42d0ce0084e0451b370a
Cam Saul <github@camsaul.com>
2021-04-28 12:51:42 -0700

    Basic select

--- test/bluejdbc/table_aware_test.clj
+++ test/bluejdbc/table_aware_test.clj
@@ -1,14 +1,30 @@
 (ns bluejdbc.table-aware-test
   (:require [bluejdbc.instance :as instance]
             [bluejdbc.table-aware :as table-aware]
+            [bluejdbc.test :as test]
             [clojure.test :refer :all]
             [java-time :as t]))
 
+(use-fixtures :once test/do-with-test-data)
+
+(defn- test-people-instances? [results]
+  (testing "All results should be :people instances"
+    (is (every? (partial = :people) (map instance/table results)))))
+
 (deftest query-as-test
   (let [results (table-aware/query-as :test/postgres :people {:select [:*], :from [:people]} nil)]
-    (is (every? (partial = :people) (map instance/table results)))
-    (is (= [(instance/instance :people {:id 1, :name "Cam", :created_at (t/offset-date-time "2020-04-21T23:56Z")})
-            (instance/instance :people {:id 2, :name "Sam", :created_at (t/offset-date-time "2019-01-11T23:56Z")})
-            (instance/instance :people {:id 3, :name "Pam", :created_at (t/offset-date-time "2020-01-01T21:56Z")})
-            (instance/instance :people {:id 4, :name "Tam", :created_at (t/offset-date-time "2020-05-25T19:56Z")})]
+    (test-people-instances? results)
+    (is (= [{:id 1, :name "Cam", :created_at (t/offset-date-time "2020-04-21T23:56Z")}
+            {:id 2, :name "Sam", :created_at (t/offset-date-time "2019-01-11T23:56Z")}
+            {:id 3, :name "Pam", :created_at (t/offset-date-time "2020-01-01T21:56Z")}
+            {:id 4, :name "Tam", :created_at (t/offset-date-time "2020-05-25T19:56Z")}]
+           results))))
+
+(deftest basic-select-test
+  (let [results (table-aware/basic-select :test/postgres :people {:select [:id]} nil)]
+    (test-people-instances? results)
+    (is (= [{:id 1}
+            {:id 2}
+            {:id 3}
+            {:id 4}]
            results))))
