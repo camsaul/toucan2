1956ac40f9bd4475f79a32a1e6bc02b88773bcea
Cam Saul <github@camsaul.com>
2021-06-01 11:26:12 -0700

    Add helper with-trace macro for debug logging

--- src/bluejdbc/honeysql_util.clj
+++ src/bluejdbc/honeysql_util.clj
@@ -4,23 +4,19 @@
             [honeysql.helpers :as hsql.helpers]))
 
 (defn merge-primary-key [kvs connectable tableable pk-vals]
-  (log/tracef "Adding primary key values %s" (pr-str pk-vals))
-  (let [pk-cols (tableable/primary-key-keys connectable tableable)
-        _       (log/tracef "Primary key(s) for %s is %s" (pr-str tableable) (pr-str pk-cols))
-        pk-vals (if (sequential? pk-vals)
-                  pk-vals
-                  [pk-vals])
-        pk-map  (zipmap pk-cols pk-vals)
-        result  (merge kvs pk-map)]
-    (log/tracef "-> %s" (pr-str result))
-    result))
+  (log/with-trace ["Adding primary key values %s" (pr-str pk-vals)]
+    (let [pk-cols (tableable/primary-key-keys connectable tableable)
+          _       (log/tracef "Primary key(s) for %s is %s" (pr-str tableable) (pr-str pk-cols))
+          pk-vals (if (sequential? pk-vals)
+                    pk-vals
+                    [pk-vals])
+          pk-map  (zipmap pk-cols pk-vals)]
+      (merge kvs pk-map))))
 
 ;; TODO -- this should probably be a multimethod, to support theoretical non-HoneySQL queries
 (defn merge-kvs [query kvs]
-  (log/tracef "Adding key-values %s" (pr-str kvs))
-  (let [query (apply hsql.helpers/merge-where query (for [[k v] kvs]
-                                                      (if (sequential? v)
-                                                        (into [(first v) k] (rest v))
-                                                        [:= k v])))]
-    (log/tracef "-> %s" (pr-str query))
-    query))
+  (log/with-trace ["Adding key-values %s" (pr-str kvs)]
+    (apply hsql.helpers/merge-where query (for [[k v] kvs]
+                                            (if (sequential? v)
+                                              (into [(first v) k] (rest v))
+                                              [:= k v])))))
