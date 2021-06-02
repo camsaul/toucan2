68bf756189480f7af033efc2099fbf06f350e22d
Cam Saul <github@camsaul.com>
2021-06-02 14:25:12 -0700

    Add :bluejdbc.experimental/insert-return-instances

---
+++ test/bluejdbc/experimental_test.clj
@@ -0,0 +1,28 @@
+(ns bluejdbc.experimental-test
+  (:require [bluejdbc.connectable.current :as conn.current]
+            [bluejdbc.experimental :as experimental]
+            [bluejdbc.instance :as instance]
+            [bluejdbc.mutative :as mutative]
+            [bluejdbc.test :as test]
+            [clojure.test :refer :all]
+            [java-time :as t]
+            [methodical.core :as m]))
+
+(comment experimental/keep-me)
+
+(derive :venues/return-instances :bluejdbc.experimental/insert-return-instances)
+
+(m/defmethod conn.current/default-connectable-for-tableable* :venues/return-instances
+  [_ _]
+  :test/postgres)
+
+(deftest insert!-return-instances-test
+  (test/with-venues-reset
+    (is (= [(instance/instance
+             :venues/return-instances
+             {:id         4
+              :name       "Grant & Green"
+              :category   "bar"
+              :created-at (t/local-date-time "2017-01-01T00:00")
+              :updated-at (t/local-date-time "2017-01-01T00:00")})]
+           (mutative/insert! :venues/return-instances {:name "Grant & Green", :category "bar"})))))
