c9888e38fbe438db2cee76aec37e18e6d1e730ab
Cam Saul <github@camsaul.com>
2021-06-09 17:03:08 -0700

    before-update should apply to all matched instances, and call f once for each.

--- src/bluejdbc/helpers.clj
+++ src/bluejdbc/helpers.clj
@@ -1,12 +1,18 @@
 (ns bluejdbc.helpers
-  (:require [bluejdbc.hydrate :as hydrate]
+  (:require [bluejdbc.compile :as compile]
+            [bluejdbc.connectable.current :as conn.current]
+            [bluejdbc.honeysql-util :as honeysql-util]
+            [bluejdbc.hydrate :as hydrate]
             [bluejdbc.instance :as instance]
             [bluejdbc.log :as log]
             [bluejdbc.mutative :as mutative]
             [bluejdbc.select :as select]
             [bluejdbc.tableable :as tableable]
             [bluejdbc.transformed :as transformed]
-            [methodical.core :as m]))
+            [bluejdbc.util :as u]
+            [clojure.walk :as walk]
+            [methodical.core :as m]
+            [methodical.impl.combo.threaded :as m.combo.threaded]))
 
 (defn dispatch-value-2 [dispatch-value]
   (let [{:keys [connectable tableable]}
@@ -78,20 +84,135 @@
 
 ;; HELPERS BELOW ARE EXPERIMENTAL/UNTESTED
 
-(defn do-before-update [connectable tableable query options f]
-  (helper "before-update" tableable
-    (update query :set (fn [values]
-                         (let [pk       (get-in options [::mutative/update :pk])
-                               instance (select/select-one [connectable tableable] pk)
-                               updated  (merge instance values)]
-                           (f updated))))))
+(m/defmulti before-update-transform-changes*
+  {:arglists '([connectable* instance* options])}
+  u/dispatch-on-first-two-args
+  :combo (m.combo.threaded/threading-method-combination :second))
+
+;; default method is a no-op
+(m/defmethod before-update-transform-changes* :default
+  [_ instance _]
+  instance)
+
+(defn reducible-instances-matching-update-query
+  "Return instances of `tableable` that match conditions from a compiled `update-query` as passed to `update!*`."
+  [connectable tableable update-query options]
+  (let [[connectable options] (conn.current/ensure-connectable connectable tableable options)
+        select-query          (-> update-query
+                                  (dissoc :update :set)
+                                  (assoc :from [(compile/table-identifier tableable options)]))]
+    (log/with-trace-no-result ["Finding matching instances with query %s" select-query]
+      (select/select-reducible [connectable tableable] select-query (or options {})))))
+
+(defn group-by-xform
+  "Transducer that groups together values into a map of `(key-fn x)` -> `[(val-fn x) ...]`, then reduces that map.
+
+    (into {} (group-by-xform even? inc) [1 2 3 4])
+    ;; ->
+    {false [2 4], true [3 5]}"
+  [key-fn val-fn]
+  (fn [rf]
+    (let [k->v (atom {})]
+      (fn
+        ([] (rf))
+        ([acc]
+         ;; optimization: remove the matching-primary-keys if there's just one set of changes to apply.
+         (let [k->v @k->v
+               k->v (if (= (bounded-count 2 k->v) 1)
+                      {(ffirst k->v) nil}
+                      k->v)]
+           (reduce
+            rf
+            acc
+            k->v)))
+        ([acc x]
+         (let [k (key-fn x)
+               v (val-fn x)]
+           (swap! k->v update k #(conj (vec %) v))
+           acc))))))
+
+#_(def ^:dynamic *update-batch-size*
+  "Maximum number of rows to fetch and update at a time when using `define-before-update` and
+  `::before-update-transform-matching-rows`."
+  100)
+
+(m/defmethod mutative/update!* :around [:default ::before-update-transform-matching-rows clojure.lang.IPersistentMap]
+  [connectable tableable {updates :set, :as query} options]
+  (log/with-trace-no-result ["Doing before-update for %s" tableable]
+    (if (empty? updates)
+      (do (log/tracef "Query %s has no changes; skipping rest of update" query)
+          0)
+      (letfn [(f [instance]
+                (before-update-transform-changes* connectable instance options))]
+        ;; TODO -- this whole thing should be done in a transaction.
+        (transduce
+         (comp
+          ;; merge in changes for each instance
+          (map (fn [instance]
+                 (log/tracef "Found matching instance %s" instance)
+                 (merge instance updates)))
+          ;; filter out instances that don't have any changes before calling `f`
+          (filter (fn [instance]
+                    (if (seq (instance/changes instance))
+                      instance
+                      (log/tracef "Skipping row with PK %s, it has no changes" (tableable/primary-key-values instance)))))
+          ;; apply the changes-xform to each instance
+          (map (fn [instance]
+                 (log/with-trace ["Apply f to %s" instance]
+                   (let [result (f instance)]
+                     (assert (instance/bluejdbc-instance? result)
+                             (format "before-update method for %s should return an instance, got ^%s %s"
+                                     (u/dispatch-value tableable)
+                                     (some-> result class .getCanonicalName)
+                                     (pr-str result)))
+                     result))))
+          ;; filter out the ones that don't have any changes AFTER calling `f`.
+          (filter (fn [instance]
+                    (if (seq (instance/changes instance))
+                      instance
+                      (log/tracef "Skipping row with PK %s, it has no changes after applying f"
+                                  (tableable/primary-key-keys instance)))))
+          ;; TODO -- consider whether we should batch the stuff below. e.g. if we end up matching 1 million objects,
+          ;; it might not be ideal to keep a million PK value vectors in memory at once. Also, a query with `UPDATE
+          ;; table WHERE id IN (...)` with a million ids probably isn't going to work so well.
+          #_(partition-all *update-batch-size*)
+          ;; Group all the PKs by their changes.
+          ;;
+          ;; TODO -- if we had a batched-update method, we wouldn't need this complicated transducer.
+          (group-by-xform instance/changes (let [pk-keys (vec (tableable/primary-key-keys connectable tableable))]
+                                             #(mapv % pk-keys)))
+          ;; do an update call for each distinct set of changes
+          (map (fn [[changes matching-primary-keys]]
+                 (let [new-query (-> query
+                                     (assoc :set changes)
+                                     (honeysql-util/merge-conditions connectable tableable
+                                                                     {:bluejdbc/with-pks matching-primary-keys}
+                                                                     options))]
+                   (log/with-trace ["Performing updates with query %s" new-query]
+                     (next-method connectable tableable new-query options))))))
+         (completing (fnil + 0 0))
+         0
+         (reducible-instances-matching-update-query connectable tableable query options))))))
+
+(defn disallow-next-method-calls [body]
+  (walk/postwalk
+   (fn [form]
+     (if (and (sequential? form)
+              (= (first form) 'next-method))
+       (throw (ex-info "Don't call next-method here. It's already called automatically!"
+                       {:body body, :form form}))
+       form))
+   body))
 
 (defmacro define-before-update {:style/indent :defn} [dispatch-value [instance-binding] & body]
-  `(m/defmethod mutative/update!* :before ~(dispatch-value-3 dispatch-value)
-     [~'&connectable ~'&tableable ~'&query ~'&options]
-     (do-before-update ~'&connectable ~'&tableable ~'&query ~'&options
-                       (fn [~instance-binding]
-                         ~@body))))
+  (let [[connectable-dv tableable-dv] (dispatch-value-2 dispatch-value)]
+    `(do
+       (when-not (isa? ~tableable-dv ::before-update-transform-matching-rows)
+         (derive ~tableable-dv ::before-update-transform-matching-rows))
+       (m/defmethod before-update-transform-changes* [~connectable-dv ~tableable-dv]
+         [~'&connectable ~instance-binding ~'&options]
+         (let [result# ~(disallow-next-method-calls `(do ~@body))]
+           (~'next-method ~'&connectable result# ~'&options))))))
 
 (defn do-after-update [connectable tableable result options f]
   (helper "after-update" tableable
