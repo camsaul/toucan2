f51c4b91ab36990ffe7a03f6eada52c1cd11cc16
Cam Saul <github@camsaul.com>
2021-06-10 21:36:27 -0700

    Move specs next to where they're actually used

--- src/bluejdbc/specs.clj
+++ src/bluejdbc/specs.clj
@@ -1,6 +1,6 @@
 (ns bluejdbc.specs
-  (:require [bluejdbc.queryable :as queryable]
-            [clojure.spec.alpha :as s]))
+  "Specs for things that are used in multiple namespaces."
+  (:require [clojure.spec.alpha :as s]))
 
 (s/def ::pk
   (every-pred (complement map?) (complement keyword?)))
@@ -12,32 +12,3 @@
 
 (s/def ::options
   map?)
-
-;; TODO -- consider whether it makes sense for these to go here as opposed to next to where they're actually used.
-
-(defn select-args-spec [connectable tableable]
-  (letfn [(query? [x]
-            (or (map? x)
-                (queryable/queryable? connectable tableable x)))]
-    ;; TODO -- rename these keys, since query is not necessarily a map.
-    (s/cat :query   (s/alt :map     (s/cat :pk         (s/? ::pk)
-                                           :conditions ::kv-conditions
-                                           :query      (s/? query?))
-
-                           :non-map (s/cat :query (s/? (complement query?))))
-           :options (s/? ::options))))
-
-(s/def ::update!-args
-  (s/cat :pk            (s/? ::pk)
-         :conditions    (s/? map?)
-         :kv-conditions ::kv-conditions
-         :changes       map?
-         :options       (s/? ::options)))
-
-(s/def ::insert!-args
-  (s/cat :rows (s/alt :single-row-map    map?
-                      :multiple-row-maps (s/coll-of map?)
-                      :kv-pairs          ::kv-conditions
-                      :columns-rows      (s/cat :columns (s/coll-of keyword?)
-                                                :rows    (s/coll-of vector?)))
-         :options (s/? ::options)))
