7d0c11ef2ae7600b1cd694e530bb1bac3c043e70
Cam Saul <1455846+camsaul@users.noreply.github.com>
2021-06-14 22:07:19 -0700

    Switch to deps.edn and subprojects (#3)
    * Big refactor: introduce the build-query API; split HoneySQL into separate backend
    
    * Improved API for buildable-query and methods.
    
    * Reorganize modules into separate folders
    
    * Some more separation of HoneySQL/JDBC code from core
    
    * Move JDBC-specific connection stuff into jdbc subproject
    
    * Switch to deps.edn
    
    * Fix config :wrench:
    
    * Fix CircleCI config
    
    * Generate .DEPS file
    
    * Generate .DEPS (fixed)
    
    * More CircleCI fixes :wrench:
    
    * Another CircleCI tweak
    
    * Another CI tweak?
    
    * CircleCI tweak
    
    * Fix CircleCI again
    
    * More CI tweaks

---
+++ deploy-all.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+set -eo pipefail
+
+script_directory=`dirname "${BASH_SOURCE[0]}"`
+cd "$script_directory"
+script_directory=`pwd`
+
+version="$1"
+
+if [ ! "$version" ]; then
+    echo "Usage: ./deploy-all.sh [version]"
+    exit -1
+fi
+
+# ./build-all.sh "$version"
+
+deploy() {
+    what="$1"
+    echo "Deploy $what"
+    cd "$script_directory"
+    rm -f pom.xml
+    cp "$script_directory/$what/pom.xml" .
+    clojure -X:deploy :artifact "\"target/$what.jar\""
+}
+
+deploy_all() {
+    deploy toucan2-core
+    deploy toucan2-honeysql
+    deploy toucan2-jdbc
+    deploy toucan2
+}
+
+deploy_all
