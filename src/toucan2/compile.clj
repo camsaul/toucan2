9f9d41104828138561b23379de442fbef63d4c30
Cam Saul <github@camsaul.com>
2022-07-21 11:09:14 -0700

    do-with-compiled-query support options

--- src/toucan2/compile.clj
+++ src/toucan2/compile.clj
@@ -4,25 +4,25 @@
             [toucan2.util :as u]))
 
 (m/defmulti do-with-compiled-query
-  {:arglists '([connection compileable f])}
+  {:arglists '([connection compileable options f])}
   u/dispatch-on-keyword-or-type-2)
 
-(defmacro with-compiled-query [[query-binding [connection compileable]] & body]
+(defmacro with-compiled-query [[query-binding [connection compileable options]] & body]
   `(do-with-compiled-query
     ~connection
     ~compileable
+    ~options
     (^:once fn* [~query-binding] ~@body)))
 
 (m/defmethod do-with-compiled-query [java.sql.Connection String]
-  [_conn sql f]
+  [_conn sql _options f]
   (f [sql]))
 
 (m/defmethod do-with-compiled-query [java.sql.Connection clojure.lang.Sequential]
-  [_conn sql-args f]
+  [_conn sql-args _options f]
   (f sql-args))
 
 (m/defmethod do-with-compiled-query [java.sql.Connection clojure.lang.IPersistentMap]
-  [conn honeysql f]
-  ;; TODO -- options
-  (let [sql-args (hsql/format honeysql)]
-    (do-with-compiled-query conn sql-args f)))
+  [conn honeysql options f]
+  (let [sql-args (hsql/format honeysql (:honeysql options))]
+    (do-with-compiled-query conn sql-args options f)))
