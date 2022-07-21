9caec431e978988aeeef048febe8b0f57723c509
Cam Saul <github@camsaul.com>
2022-07-21 14:12:25 -0700

    Debug results

--- src/toucan2/util.clj
+++ src/toucan2/util.clj
@@ -1,4 +1,33 @@
-(ns toucan2.util)
+(ns toucan2.util
+  (:require [clojure.string :as str]))
+
+(def ^:dynamic *debug* false)
+
+(def ^:dynamic ^:private *recursive-debug-result-level* 0)
+
+(defn println-debug* [s]
+  (let [lines       (str/split-lines (str/trim s))
+        indentation (str/join (repeat *recursive-debug-result-level* "  "))]
+    (doseq [line lines]
+      (print indentation)
+      (println line))))
+
+(defmacro println-debug [& args]
+  `(when *debug*
+     (println-debug* (with-out-str (println ~@args)))))
+
+(defn do-with-debug-result [message thunk]
+  (println-debug message)
+  (let [result (binding [*recursive-debug-result-level* (inc *recursive-debug-result-level*)]
+                 (thunk))]
+    (println-debug '=> (pr-str result))
+    result))
+
+(defmacro with-debug-result [message & body]
+  `(let [thunk# (^:once fn* [] ~@body)]
+     (if-not *debug*
+       (thunk#)
+       (do-with-debug-result ~message thunk#))))
 
 (defn keyword-or-type [x]
   (if (keyword? x)
---
+++ test/toucan2/util_test.clj
@@ -0,0 +1,69 @@
+(ns toucan2.util-test
+  (:require [toucan2.util :as u]
+            [clojure.test :refer :all]
+            [clojure.string :as str]))
+
+(defmacro ^:private out-str-lines [& body]
+  `(some-> (with-out-str ~@body)
+           str/trim
+           not-empty
+           str/split-lines))
+
+(defmacro ^:private println-debug [& args])
+
+(deftest println-debug-test
+  (testing "debugging disabled"
+    (let [evaled? (atom false)]
+      (testing "don't print anything"
+        (is (= nil
+               (out-str-lines (u/println-debug (do (reset! evaled? true) "wow"))))))
+      (testing "don't eval args"
+        (is (= false
+               @evaled?)))))
+  (testing "debugging enabled"
+    (binding [u/*debug* true]
+      (let [evaled? (atom false)]
+        (is (= ["wow"]
+               (out-str-lines (u/println-debug (do (reset! evaled? true) "wow")))))
+        (is (= true
+               @evaled?))))))
+
+(deftest with-debug-result-test
+  (testing "debugging disabled"
+    (let [message-form-evaled? (atom false)]
+      (testing "don't print anything"
+        (is (= nil
+               (out-str-lines
+                (u/with-debug-result (do
+                                       (reset! message-form-evaled? true)
+                                       "DEBUG")
+                  {:cans (+ 1 1)})))))
+      (testing "don't eval message form"
+        (is (= false
+               @message-form-evaled?)))))
+  (testing "debugging enabled"
+    (binding [u/*debug* true]
+      (let [message-form-evaled? (atom false)]
+        (is (= ["DEBUG"
+                "=> {:cans 2}"]
+               (out-str-lines
+                (u/with-debug-result (do
+                                       (reset! message-form-evaled? true)
+                                       "DEBUG")
+                  {:cans (+ 1 1)}))))
+        (is (= true
+               @message-form-evaled?)))))
+  (testing "Nested debug results"
+    (binding [u/*debug* true]
+      (is (= ["A"
+              "  B"
+              "  => 2"
+              "  C"
+              "  => :cans"
+              "=> [2 :cans]"]
+             (out-str-lines
+              (u/with-debug-result "A"
+                [(u/with-debug-result "B"
+                   2)
+                 (u/with-debug-result "C"
+                   :cans)])))))))
