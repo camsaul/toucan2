a29560dcb3ef97dc25bbf87dba17c2d7d075de9b
Cam Saul <github@camsaul.com>
2022-07-22 18:23:15 -0700

    Implement arbitrary condition support and more select-* methods

--- test/toucan2/select_test.clj
+++ test/toucan2/select_test.clj
@@ -3,29 +3,35 @@
    [clojure.test :refer :all]
    [toucan2.select :as select]
    [toucan2.test :as test]
-   [toucan2.instance :as instance]))
+   [toucan2.instance :as instance]
+   [clojure.string :as str]
+   [methodical.core :as m]
+   [toucan2.model :as model])
+  (:import java.time.OffsetDateTime))
 
-(deftest ^:parallel parse-select-args-test
+(derive ::people ::test/people)
+
+(deftest ^:parallel parse-args-test
   (doseq [[args expected] {[1]
-                           {:query 1}
+                           [{} 1]
 
                            [:id 1]
-                           {:conditions {:id 1}, :query {}}
+                           [{:id 1} {}]
 
                            [{:where [:= :id 1]}]
-                           {:query {:where [:= :id 1]}}
+                           [{} {:where [:= :id 1]}]
 
                            [:name "Cam" {:where [:= :id 1]}]
-                           {:conditions {:name "Cam"}, :query {:where [:= :id 1]}}
+                           [{:name "Cam"} {:where [:= :id 1]}]
 
                            [::my-query]
-                           {:query ::my-query}}]
-    (testing `(select/parse-select-args ~args)
+                           [{} ::my-query]}]
+    (testing `(select/parse-args :default ~args)
       (is (= expected
-             (select/parse-select-args args))))))
+             (select/parse-args :default args))))))
 
 (deftest select-test
-  (let [expected [(instance/instance ::test/people {:id 1, :name "Cam", :created-at (java.time.OffsetDateTime/parse "2020-04-21T23:56Z")})]]
+  (let [expected [(instance/instance ::test/people {:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56Z")})]]
     (testing "plain SQL"
       (is (= expected
              (select/select ::test/people "SELECT * FROM people WHERE id = 1;"))))
@@ -45,122 +51,124 @@
       (is (= [(instance/instance ::test/people {:id 1})]
              (select/select [::test/people :id] :id 1))))))
 
-#_(deftest select-test
-  (let [all-rows [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}
-                  {:id 2, :name "Sam", :created-at (t/offset-date-time "2019-01-11T23:56:00Z")}
-                  {:id 3, :name "Pam", :created-at (t/offset-date-time "2020-01-01T21:56:00Z")}
-                  {:id 4, :name "Tam", :created-at (t/offset-date-time "2020-05-25T19:56:00Z")}]]
+(deftest select-test-2
+  (let [all-rows [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}
+                  {:id 2, :name "Sam", :created-at (OffsetDateTime/parse "2019-01-11T23:56:00Z")}
+                  {:id 3, :name "Pam", :created-at (OffsetDateTime/parse "2020-01-01T21:56:00Z")}
+                  {:id 4, :name "Tam", :created-at (OffsetDateTime/parse "2020-05-25T19:56:00Z")}]]
     (testing "no args"
       (is (= all-rows
              (sort-by :id (select/select ::people))))
       (is (= all-rows
              (sort-by :id (select/select ::test/people))))
-      (test-people-instances? (select/select ::test/people {:order-by [[:id :asc]]})))
-    (testing "using current connection (dynamic binding)"
-      (binding [conn.current/*current-connectable* :test/postgres]
-        (is (= all-rows
-               (select/select :people {:order-by [[:id :asc]]})))))
-    (testing "using default connection"
-      (test/with-default-connection
-        (is (= all-rows
-               (select/select :people {:order-by [[:id :asc]]})))))
-    (testing "with default connectable for tableable"
-      (is (= all-rows
-             (select/select ::people {:order-by [[:id :asc]]})))))
-
+      (is (every? #(instance/instance-of? % ::test/people)
+                  (select/select ::test/people {:order-by [[:id :asc]]})))))
   (testing "one arg (id)"
-    (is (= [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}]
+    (is (= [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}]
            (select/select ::people 1))))
-
   (testing "one arg (query)"
-    (is (= [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}]
+    (is (= [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}]
            (select/select ::test/people {:where [:= :id 1]})))
     (is (= [{:id 1, :name "Tempest", :category "bar"}]
-           (select/select [:test/postgres :venues] {:select [:id :name :category], :limit 1, :where [:= :id 1]}))))
-
+           (select/select ::test/venues {:select [:id :name :category], :limit 1, :where [:= :id 1]}))))
   (testing "two args (k v)"
-    (is (= [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}]
+    (is (= [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}]
            (select/select ::test/people :id 1)))
     (testing "sequential v"
-      (is (= [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}]
+      (is (= [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}]
              (select/select ::test/people :id [:= 1]))))))
 
-#_(m/defmethod tableable/primary-key* [:default :people/name-is-pk]
-  [_ _]
+(m/defmethod model/table-name ::people
+  [_model]
+  "people")
+
+(derive ::people.name-is-pk ::people)
+
+(m/defmethod model/primary-keys ::people.name-is-pk
+  [_model]
   :name)
 
-#_(m/defmethod tableable/primary-key* [:default :people/composite-pk]
-  [_ _]
+(derive ::people.composite-pk ::people)
+
+(m/defmethod model/primary-keys ::people.composite-pk
+  [_model]
   [:id :name])
 
-#_(deftest select-non-integer-pks-test
+(deftest select-non-integer-pks-test
   (testing "non-integer PK"
-    (is (= [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}]
-           (select/select [:test/postgres :people/name-is-pk] "Cam"))))
+    (is (= [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}]
+           (select/select ::people.name-is-pk :toucan2/pk "Cam"))))
 
   (testing "composite PK"
-    (is (= [{:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56:00Z")}]
-           (select/select [:test/postgres :people/composite-pk] [1 "Cam"])))
+    (is (= [{:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56:00Z")}]
+           (select/select ::people.composite-pk :toucan2/pk [1 "Cam"])))
     (is (= []
-           (select/select [:test/postgres :people/composite-pk] [2 "Cam"])
-           (select/select [:test/postgres :people/composite-pk] [1 "Sam"])))))
+           (select/select ::people.composite-pk :toucan2/pk [2 "Cam"])
+           (select/select ::people.composite-pk :toucan2/pk [1 "Sam"])))))
+
+(derive ::people.no-timestamps ::people)
 
 ;; this could also be done as part a `:before` method.
-#_(m/defmethod select/select* [:default :people/no-timestamps :toucan2/honeysql]
-  [connectable tableable query options]
-  (let [query (assoc query :select [:id :name])]
-    (next-method connectable tableable query options)))
+(m/defmethod select/select-reducible* ::people.no-timestamps
+  [model columns conditions query]
+  (let [columns (or columns [:id :name])]
+    (next-method model columns conditions query)))
 
-#_(deftest default-query-test
-  (testing "Should be able to set some defaults by implementing `select*`"
-    (test/with-default-connection
-      (is (= [(instance/instance :people/no-timestamps {:id 1, :name "Cam"})]
-             (select/select :people/no-timestamps 1))))))
-
-#_(m/defmethod select/select* :before [:default :people/limit-2 :toucan2/honeysql]
-  [_ _ query _]
-  (assoc query :limit 2))
-
-#_(deftest pre-select-test
-  (testing "Should be able to do cool stuff in pre-select (select* :before)"
-    (test/with-default-connection
-      (is (= [(instance/instance :people/limit-2 {:id 1, :name "Cam", :created-at (t/offset-date-time "2020-04-21T23:56Z")})
-              (instance/instance :people/limit-2 {:id 2, :name "Sam", :created-at (t/offset-date-time "2019-01-11T23:56Z")})]
-             (select/select :people/limit-2))))))
-
-#_(m/defmethod select/select* :after [:default :people/no-timestamps :default]
-  [connectable tableable reducible-query options]
+(m/defmethod select/select-reducible* :after ::people.no-timestamps
+  [_model _columns _conditions reducible-query]
   (testing "should not be an eduction yet -- if it is it means this method is getting called more than once"
     (is (not (instance? clojure.core.Eduction reducible-query))))
   (assert (not (instance? clojure.core.Eduction reducible-query)))
   (eduction
    (map (fn [person]
           (testing "select* :after should see Toucan 2 instances"
-            (is (instance/toucan2-instance? person)))
-          (testing "instance table should be a :people/no-timestamps"
-            (is (isa? (instance/tableable person) :people/no-timestamps)))
-          (dissoc person :timestamp)))
+            (is (instance/instance? person)))
+          (testing "instance table should be a ::people.no-timestamps"
+            (is (isa? (instance/model person) ::people.no-timestamps)))
+          (assoc person :after-select? true)))
    reducible-query))
 
-#_(deftest post-select-test
+(deftest default-query-test
+  (testing "Should be able to set some defaults by implementing `select*`"
+    (is (= [(instance/instance ::people.no-timestamps {:id 1, :name "Cam", :after-select? true})]
+           (select/select ::people.no-timestamps 1)))))
+
+(deftest post-select-test
   (testing "Should be able to do cool stuff in (select* :after)"
-    (test/with-default-connection
-      (is (= [(instance/instance :people/no-timestamps {:id 1, :name "Cam"})
-              (instance/instance :people/no-timestamps {:id 2, :name "Sam"})
-              (instance/instance :people/no-timestamps {:id 3, :name "Pam"})
-              (instance/instance :people/no-timestamps {:id 4, :name "Tam"})]
-             (select/select :people/no-timestamps))))))
+    (testing (str \newline '(ancestors ::people.no-timestamps) " => " (pr-str (ancestors ::people.no-timestamps)))
+      (is (= [(instance/instance ::people.no-timestamps {:id 1, :name "Cam", :after-select? true})
+              (instance/instance ::people.no-timestamps {:id 2, :name "Sam", :after-select? true})
+              (instance/instance ::people.no-timestamps {:id 3, :name "Pam", :after-select? true})
+              (instance/instance ::people.no-timestamps {:id 4, :name "Tam", :after-select? true})]
+             (select/select ::people.no-timestamps))))))
 
-#_(derive :people/no-timestamps-limit-2 :people/no-timestamps)
-#_(derive :people/no-timestamps-limit-2 :people/limit-2)
+(derive ::people.limit-2 ::people)
 
-#_(deftest combine-aux-methods-test
-  (is (= [(instance/instance :people/no-timestamps-limit-2 {:id 1, :name "Cam"})
-          (instance/instance :people/no-timestamps-limit-2 {:id 2, :name "Sam"})]
-         (select/select [:test/postgres :people/no-timestamps-limit-2]))))
+;; TODO this is probably not the way you'd want to accomplish this in real life -- I think you'd probably actually want
+;; to implement [[toucan2.model/build-select-query]] for `[::people.limit-2 clojure.lang.IPersistentMap]` instead. But
+;; it does do a good job of letting us test that combining aux methods work like we'd expect.
+(m/defmethod select/select-reducible* :before ::people.limit-2
+  [_model _columns _conditions query]
+  (cond-> query
+    (map? query) (assoc :limit 2)))
+
+(deftest pre-select-test
+  (testing "Should be able to do cool stuff in pre-select (select* :before)"
+    (is (= [(instance/instance ::people.limit-2 {:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56Z")})
+            (instance/instance ::people.limit-2 {:id 2, :name "Sam", :created-at (OffsetDateTime/parse "2019-01-11T23:56Z")})]
+           (select/select ::people.limit-2)))))
+
+(derive ::people.no-timestamps-limit-2 ::people.no-timestamps)
+(derive ::people.no-timestamps-limit-2 ::people.limit-2)
+
+(deftest combine-aux-methods-test
+  (testing (str \newline '(ancestors ::people.no-timestamps-limit-2) " => " (pr-str (ancestors ::people.no-timestamps-limit-2)))
+    (is (= [(instance/instance ::people.no-timestamps-limit-2 {:id 1, :name "Cam", :after-select? true})
+            (instance/instance ::people.no-timestamps-limit-2 {:id 2, :name "Sam", :after-select? true})]
+           (select/select ::people.no-timestamps-limit-2)))))
 
 (deftest select-one-test
-  (is (= (instance/instance ::test/people {:id 1, :name "Cam", :created-at (java.time.OffsetDateTime/parse "2020-04-21T23:56Z")})
+  (is (= (instance/instance ::test/people {:id 1, :name "Cam", :created-at (OffsetDateTime/parse "2020-04-21T23:56Z")})
          (select/select-one ::test/people 1)))
   (is (= nil
          (select/select-one ::test/people :id 1000))))
@@ -178,49 +186,49 @@
   (testing "Should work with magical keys"
     (doseq [k [:created-at :created_at]]
       (testing k
-        (is (= [(java.time.OffsetDateTime/parse "2020-04-21T23:56Z")
-                (java.time.OffsetDateTime/parse "2019-01-11T23:56Z")
-                (java.time.OffsetDateTime/parse "2020-01-01T21:56Z")
-                (java.time.OffsetDateTime/parse "2020-05-25T19:56Z")]
+        (is (= [(OffsetDateTime/parse "2020-04-21T23:56Z")
+                (OffsetDateTime/parse "2019-01-11T23:56Z")
+                (OffsetDateTime/parse "2020-01-01T21:56Z")
+                (OffsetDateTime/parse "2020-05-25T19:56Z")]
                (select/select-fn-vec k ::test/people {:order-by [[:id :asc]]}))))))
   (testing "Should return nil if the result is empty"
     (is (nil? (select/select-fn-set :id ::test/people :id 100)))
     (is (nil? (select/select-fn-vec :id ::test/people :id 100)))))
 
-#_(deftest select-one-fn-test
+(deftest select-one-fn-test
   (is (= 1
          (select/select-one-fn :id ::test/people :name "Cam"))))
 
-#_(deftest select-pks-test
+(deftest select-pks-test
   (is (= #{1 2 3 4}
          (select/select-pks-set ::test/people)))
   (is (= [1 2 3 4]
          (select/select-pks-vec ::test/people)))
   (testing "non-integer PK"
     (is (= #{"Cam" "Sam" "Pam" "Tam"}
-           (select/select-pks-set [:test/postgres :people/name-is-pk])))
+           (select/select-pks-set ::people.name-is-pk)))
     (is (= ["Cam" "Sam" "Pam" "Tam"]
-           (select/select-pks-vec [:test/postgres :people/name-is-pk]))))
+           (select/select-pks-vec ::people.name-is-pk))))
   (testing "Composite PK -- should return vectors"
     (is (= #{[1 "Cam"] [2 "Sam"] [3 "Pam"] [4 "Tam"]}
-           (select/select-pks-set [:test/postgres :people/composite-pk])))
+           (select/select-pks-set ::people.composite-pk)))
     (is (= [[1 "Cam"] [2 "Sam"] [3 "Pam"] [4 "Tam"]]
-           (select/select-pks-vec [:test/postgres :people/composite-pk]))))
+           (select/select-pks-vec ::people.composite-pk))))
   (testing "Should return nil if the result is empty"
     (is (nil? (select/select-pks-set ::test/people :id 100)))
     (is (nil? (select/select-pks-vec ::test/people :id 100)))))
 
-#_(deftest select-one-pk-test
+(deftest select-one-pk-test
   (is (= 1
          (select/select-one-pk ::test/people :name "Cam")))
   (testing "non-integer PK"
     (is (= "Cam"
-           (select/select-one-pk [:test/postgres :people/name-is-pk] :id 1))))
+           (select/select-one-pk ::people.name-is-pk :id 1))))
   (testing "Composite PK -- should return vector"
     (is (= [1 "Cam"]
-           (select/select-one-pk [:test/postgres :people/composite-pk] :id 1)))))
+           (select/select-one-pk ::people.composite-pk :id 1)))))
 
-#_(deftest select-fn->fn-test
+(deftest select-fn->fn-test
   (is (= {1 "Cam", 2 "Sam", 3 "Pam", 4 "Tam"}
          (select/select-fn->fn :id :name ::test/people)))
   (is (= {2 "cam", 3 "sam", 4 "pam", 5 "tam"}
@@ -228,18 +236,18 @@
   (testing "Should return nil if the result is empty"
     (is (nil? (select/select-fn->fn :id :name ::test/people :id 100)))))
 
-#_(deftest select-pk->fn-test
+(deftest select-pk->fn-test
   (is (= {1 "Cam", 2 "Sam", 3 "Pam", 4 "Tam"}
          (select/select-pk->fn :name ::test/people)))
   (is (= {1 "cam", 2 "sam", 3 "pam", 4 "tam"}
          (select/select-pk->fn (comp str/lower-case :name) ::test/people)))
   (testing "Composite PKs"
     (is (= {[1 "Cam"] "Cam", [2 "Sam"] "Sam", [3 "Pam"] "Pam", [4 "Tam"] "Tam"}
-           (select/select-pk->fn :name [:test/postgres :people/composite-pk]))))
+           (select/select-pk->fn :name ::people.composite-pk))))
   (testing "Should return nil if the result is empty"
     (is (nil? (select/select-pk->fn :name ::test/people :id 100)))))
 
-#_(deftest select-fn->pk-test
+(deftest select-fn->pk-test
   (is (= {"Cam" 1, "Sam" 2, "Pam" 3, "Tam" 4}
          (select/select-fn->pk :name ::test/people)))
   (is (= {"cam" 1, "sam" 2, "pam" 3, "tam" 4}
@@ -247,8 +255,8 @@
   (testing "Composite PKs"
     (is (= {"Cam" [1 "Cam"], "Sam" [2 "Sam"], "Pam" [3 "Pam"], "Tam" [4 "Tam"]}
   (testing "Should return nil if the result is empty"
-           (select/select-fn->pk :name [:test/postgres :people/composite-pk]))))
-    (is (nil? (select/select-fn->pk :name [:test/postgres :people/composite-pk] :id 100)))))
+           (select/select-fn->pk :name ::people.composite-pk))))
+    (is (nil? (select/select-fn->pk :name ::people.composite-pk :id 100)))))
 
 #_(deftest count-test
   (is (= 4
@@ -256,9 +264,9 @@
   (is (= 1
          (select/count ::test/people 1)))
   (is (= 3
-         (select/count [:test/postgres :venues])))
+         (select/count ::test/venues)))
   (is (= 2
-         (select/count [:test/postgres :venues] :category "bar"))))
+         (select/count ::test/venues :category "bar"))))
 
 #_(deftest exists?-test
   (is (= true
@@ -298,13 +306,13 @@
   [_ _ _ m _ metta]
   (with-meta m metta))
 
-#_(m/defmethod conn.current/default-connectable-for-tableable* :people/custom-instance-type
+#_(m/defmethod conn.current/default-connectable-for-model* :people/custom-instance-type
   [_ _]
   :test/postgres)
 
 #_(deftest custom-instance-type-test
   (let [m (select/select-one :people/custom-instance-type 1)]
-    (is (= {:id 1, :name "Cam", :created_at (t/offset-date-time "2020-04-21T23:56Z")}
+    (is (= {:id 1, :name "Cam", :created_at (OffsetDateTime/parse "2020-04-21T23:56Z")}
            m))
     (is (map? m))
     (is (not (instance/toucan2-instance? m)))))
