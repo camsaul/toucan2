3ed55c540e3ca29e57bf51453e8f0aa8d4477584
Cam Saul <github@camsaul.com>
2022-08-17 23:24:22 -0700

    tools.disallow

--- src/toucan2/tools/disallow.clj
+++ src/toucan2/tools/disallow.clj
@@ -1,21 +1,23 @@
 (ns toucan2.tools.disallow
-  #_(:require [methodical.core :as m]
-            [toucan2.util :as u]
-            [toucan2.select :as select]
-            [toucan2.mutative :as mutative]))
+  (:require
+   [methodical.core :as m]
+   [toucan2.delete :as delete]
+   [toucan2.insert :as insert]
+   [toucan2.select :as select]
+   [toucan2.update :as update]))
 
-;; (m/defmethod select/select* [:default :toucan/disallow-select :default]
-;;   [_ tableable _ _]
-;;   (throw (UnsupportedOperationException. (format "You cannot select %s." (u/dispatch-value tableable)))))
+(m/defmethod select/select-reducible* ::select
+  [model _parsed-args]
+  (throw (UnsupportedOperationException. (format "You cannot select %s." model))))
 
-;; (m/defmethod mutative/delete!* [:default :toucan/disallow-delete :default]
-;;   [_ tableable _ _]
-;;   (throw (UnsupportedOperationException. (format "You cannot delete instances of %s." (u/dispatch-value tableable)))))
+(m/defmethod delete/delete!* ::delete
+  [model _parsed-args]
+  (throw (UnsupportedOperationException. (format "You cannot delete instances of %s." model))))
 
-;; (m/defmethod mutative/insert!* [:default :toucan/disallow-insert :default]
-;;   [_ tableable _ _]
-;;   (throw (UnsupportedOperationException. (format "You cannot create new instances of %s." (u/dispatch-value tableable)))))
+(m/defmethod insert/reducible-insert* ::insert
+  [model _parsed-args]
+  (throw (UnsupportedOperationException. (format "You cannot create new instances of %s." model))))
 
-;; (m/defmethod mutative/update!* [:default :toucan/disallow-update :default]
-;;   [_ tableable _ _]
-;;   (throw (UnsupportedOperationException. (format "You cannot update a %s after it has been created." (u/dispatch-value tableable)))))
+(m/defmethod update/reducible-update* ::update
+  [model _parsed-args]
+  (throw (UnsupportedOperationException. (format "You cannot update a %s after it has been created." model))))
