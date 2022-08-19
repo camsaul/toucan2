f8925e4b921f4d6002759e03dd6e4776bec4ca6f
Cam Saul <github@camsaul.com>
2022-08-18 19:37:09 -0700

    Make do-with-temp* a multimethod to provide a customization point

--- src/toucan2/tools/with_temp.clj
+++ src/toucan2/tools/with_temp.clj
@@ -16,27 +16,43 @@
   [_model]
   nil)
 
+(m/defmulti do-with-temp*
+  "Implementation of [[with-temp]]. You can implement this if you need to do some sort of special behavior for a
+  particular model. But normally you would just implement [[with-temp-defaults]]. If you need to do special setup when
+  using [[with-temp]], you can implement a `:before` method:
+
+    (m/defmethod do-with-temp* :before :default
+      [model attributes f]
+      (set-up-db!)
+      f)"
+  {:arglists '([model attributes f])}
+  u/dispatch-on-first-arg)
+
+(m/defmethod do-with-temp* :default
+  [model attributes f]
+  (let [defaults          (with-temp-defaults model)
+        merged-attributes (merge {} defaults attributes)
+        [pk temp-object]  (u/with-debug-result ["Create temporary %s with attributes %s" model merged-attributes]
+                            (let [[pk] (try
+                                         (insert/insert-returning-pks! model merged-attributes)
+                                         (catch Throwable e
+                                           (throw (ex-info (format "Error inserting temp %s: %s" (pr-str model) (ex-message e))
+                                                           {:model  model
+                                                            :attributes {:parameters attributes
+                                                                         :default    defaults
+                                                                         :merged     merged-attributes}}
+                                                           e))))]
+                              [pk (select/select-one model pk)]))]
+
+    (try
+      (t/testing (format "with temporary %s with attributes %s" (pr-str model) (pr-str merged-attributes))
+        (f temp-object))
+      (finally
+        (delete/delete! model pk)))))
+
 (defn do-with-temp [modelable attributes f]
   (model/with-model [model modelable]
-    (let [defaults          (with-temp-defaults model)
-          merged-attributes (merge {} defaults attributes)
-          [pk temp-object]  (u/with-debug-result ["Create temporary %s with attributes %s" model merged-attributes]
-                              (let [[pk] (try
-                                           (insert/insert-returning-pks! model merged-attributes)
-                                           (catch Throwable e
-                                             (throw (ex-info (format "Error inserting temp %s: %s" (pr-str model) (ex-message e))
-                                                             {:model  model
-                                                              :attributes {:parameters attributes
-                                                                           :default    defaults
-                                                                           :merged     merged-attributes}}
-                                                             e))))]
-                                [pk (select/select-one model pk)]))]
-
-      (try
-        (t/testing (format "with temporary %s with attributes %s" (pr-str model) (pr-str merged-attributes))
-          (f temp-object))
-        (finally
-          (delete/delete! model pk))))))
+    (do-with-temp* model attributes f)))
 
 (defmacro with-temp [[modelable temp-object-binding attributes & more] & body]
   `(do-with-temp ~modelable ~attributes
