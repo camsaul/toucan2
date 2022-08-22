f218b63fec247918b602354f96bcca4e213d9e60
Cam Saul <github@camsaul.com>
2022-08-21 21:43:03 -0700

    Add Clj-Kondo config hooks for with-connection and with-transaction

--- test/toucan2/connection_test.clj
+++ test/toucan2/connection_test.clj
@@ -1,7 +1,11 @@
 (ns toucan2.connection-test
   (:require
    [clojure.test :refer :all]
-   [toucan2.connection :as conn]))
+   [toucan2.connection :as conn]
+   [toucan2.current :as current]
+   [toucan2.test :as test]))
+
+(set! *warn-on-reflection* true)
 
 (deftest ^:parallel connection-string-protocol-test
   (doseq [[s expected] {"jdbc:postgres://localhost:5432/my_db" "jdbc"
@@ -14,12 +18,27 @@
              (conn/connection-string-protocol s))))))
 
 (deftest with-connection-test
-  (conn/with-connection [conn-from-keyword ::test/db]
+  (letfn [(test-current-connection-bound? []
+            (testing "current connectable should be bound"
+              (is (instance? java.sql.Connection current/*connectable*))))
+          (test-connection [conn]
+            (is (instance? java.sql.Connection conn))
+            (test-current-connection-bound?))]
     (testing "Connection from keyword"
-      (is (instance? java.sql.Connection conn-from-keyword)))
-    (conn/with-connection [conn-from-conn conn-from-keyword]
-      (testing "Connection from Connection"
-        (is (instance? java.sql.Connection conn-from-conn))))))
+      (conn/with-connection [conn-from-keyword ::test/db]
+        (test-connection conn-from-keyword)
+        (testing "Connection from Connection"
+          (conn/with-connection [conn-from-conn conn-from-keyword]
+            (test-connection conn-from-conn)))))
+    (testing "nil connectable = current connection"
+      (testing "nil second arg"
+        (binding [current/*connectable* ::test/db]
+          (conn/with-connection [conn nil]
+            (test-connection conn)))
+        (testing "no second arg"
+          (binding [current/*connectable* ::test/db]
+            (conn/with-connection [conn]
+              (test-connection conn))))))))
 
 (deftest jdbc-spec-test
   (conn/with-connection [conn {:dbtype   "h2:mem"
