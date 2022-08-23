fcafe3e3bad028db5fd1de044cbb72c5cbeef4b7
Cam Saul <github@camsaul.com>
2022-08-22 20:33:32 -0700

    Tests for tools.disallow

--- src/toucan2/tools/disallow.clj
+++ src/toucan2/tools/disallow.clj
@@ -4,17 +4,17 @@
    [toucan2.operation :as op]))
 
 (m/defmethod op/reducible-returning-instances* [:toucan2.select/select ::select]
-  [model _parsed-args]
+  [_query-type model _parsed-args]
   (throw (UnsupportedOperationException. (format "You cannot select %s." model))))
 
 (m/defmethod op/reducible* [:toucan2.delete/delete ::delete]
-  [model _parsed-args]
+  [_query-type model _parsed-args]
   (throw (UnsupportedOperationException. (format "You cannot delete instances of %s." model))))
 
 (m/defmethod op/reducible* [:toucan2.insert/insert ::insert]
-  [model _parsed-args]
+  [_query-type model _parsed-args]
   (throw (UnsupportedOperationException. (format "You cannot create new instances of %s." model))))
 
 (m/defmethod op/reducible* [:toucan2.update/update ::update]
-  [model _parsed-args]
+  [_query-type model _parsed-args]
   (throw (UnsupportedOperationException. (format "You cannot update a %s after it has been created." model))))
--- test/toucan2/tools/disallow_test.clj
+++ test/toucan2/tools/disallow_test.clj
@@ -1,54 +1,48 @@
 (ns toucan2.tools.disallow-test
-  #_(:require [clojure.test :refer :all]
-            [methodical.core :as m]
-            [toucan2.mutative :as mutative]
-            [toucan2.select :as select]
-            [toucan2.tableable :as tableable]
-            [toucan2.test :as test]
-            toucan2.tools.disallow))
+  (:require
+   [clojure.test :refer :all]
+   [toucan2.delete :as delete]
+   [toucan2.insert :as insert]
+   [toucan2.select :as select]
+   [toucan2.test :as test]
+   [toucan2.tools.disallow :as disallow]
+   [toucan2.update :as update]))
 
-;; (comment toucan2.tools.disallow/keep-me)
-
-;; (use-fixtures :once test/do-with-test-data)
-;; (use-fixtures :each test/do-with-default-connection)
-
-;; (m/defmethod tableable/table-name* [:default ::venues]
-;;   [_ _ _]
-;;   "venues")
-
-;; (derive ::venues-no-select ::venues)
-;; (derive ::venues-no-select :toucan/disallow-select)
-
-;; (deftest disallow-select-test
-;;   (is (thrown-with-msg?
-;;        Exception
-;;        #"You cannot select :toucan2.tools.disallow-test/venues-no-select"
-;;        (select/select ::venues-no-select))))
+(use-fixtures :each test/do-db-types-fixture)
 
 
-;; (derive ::venues-no-delete ::venues)
-;; (derive ::venues-no-delete :toucan/disallow-delete)
+(derive ::venues.no-select ::test/venues)
+(derive ::venues.no-select ::disallow/select)
 
-;; (deftest disallow-delete-test
-;;   (is (thrown-with-msg?
-;;        Exception
-;;        #"You cannot delete instances of :toucan2.tools.disallow-test/venues-no-delete"
-;;        (mutative/delete! ::venues-no-delete))))
+(deftest disallow-select-test
+  (is (thrown-with-msg?
+       UnsupportedOperationException
+       #"You cannot select :toucan2.tools.disallow-test/venues.no-select"
+       (select/select ::venues.no-select))))
 
-;; (derive ::venues-no-insert ::venues)
-;; (derive ::venues-no-insert :toucan/disallow-insert)
+(derive ::venues.no-delete ::test/venues)
+(derive ::venues.no-delete ::disallow/delete)
 
-;; (deftest disallow-insert-test
-;;   (is (thrown-with-msg?
-;;        Exception
-;;        #"You cannot create new instances of :toucan2.tools.disallow-test/venues-no-insert"
-;;        (mutative/insert! ::venues-no-insert {:a 1, :b 2}))))
+(deftest disallow-delete-test
+  (is (thrown-with-msg?
+       UnsupportedOperationException
+       #"You cannot delete instances of :toucan2.tools.disallow-test/venues.no-delete"
+       (delete/delete! ::venues.no-delete))))
 
-;; (derive ::venues-no-update ::venues)
-;; (derive ::venues-no-update :toucan/disallow-update)
+(derive ::venues.no-insert ::test/venues)
+(derive ::venues.no-insert ::disallow/insert)
 
-;; (deftest disallow-update-test
-;;   (is (thrown-with-msg?
-;;        Exception
-;;        #"You cannot update a :toucan2.tools.disallow-test/venues-no-update after it has been created"
-;;        (mutative/update! ::venues-no-update 1 {:a 1, :b 2}))))
+(deftest disallow-insert-test
+  (is (thrown-with-msg?
+       UnsupportedOperationException
+       #"You cannot create new instances of :toucan2.tools.disallow-test/venues.no-insert"
+       (insert/insert! ::venues.no-insert {:a 1, :b 2}))))
+
+(derive ::venues.no-update ::test/venues)
+(derive ::venues.no-update ::disallow/update)
+
+(deftest disallow-update-test
+  (is (thrown-with-msg?
+       UnsupportedOperationException
+       #"You cannot update a :toucan2.tools.disallow-test/venues.no-update after it has been created"
+       (update/update! ::venues.no-update 1 {:a 1, :b 2}))))
