75342e7d08aa90902d4bbceeefc5d6fff816e7bd
Cam Saul <github@camsaul.com>
2022-08-22 22:24:20 -0700

    Set up deployment to Clojars

---
+++ toucan1/build.clj
@@ -0,0 +1,20 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
+
+(def lib 'io.github.camsaul/toucan2-toucan1)
+
+;;; if you want a version of MAJOR.MINOR.COMMITS:
+(def version (format "0.9.%s" (b/git-count-revs nil)))
+
+(def options
+  {:lib lib, :version version})
+
+(println "Options:" (pr-str options))
+
+(defn build [& _]
+  (bb/clean options)
+  (bb/jar options))
+
+(defn deploy [& _]
+  (bb/deploy options))
