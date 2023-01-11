5260fb79a65148d768f1f91f183d8f3080550ffd
Cam Saul <1455846+camsaul@users.noreply.github.com>
2023-01-10 18:42:28 -0800

    Fix default fields in combination with after select (#48)

--- src/toucan2/tools/after_select.clj
+++ src/toucan2/tools/after_select.clj
@@ -1,17 +1,30 @@
 (ns toucan2.tools.after-select
   (:require
    [clojure.spec.alpha :as s]
-   [toucan2.tools.simple-out-transform :as tools.simple-out-transform]))
+   [methodical.core :as m]
+   [toucan2.tools.simple-out-transform :as tools.simple-out-transform]
+   [toucan2.util :as u]))
+
+(m/defmulti after-select
+  {:arglists '([instance])}
+  u/dispatch-on-first-arg)
+
+(tools.simple-out-transform/define-out-transform [:toucan.query-type/select.instances ::after-select]
+  [instance]
+  ;; don't do after-select if this select is a result of doing something like insert-returning instances
+  (if (isa? &query-type :toucan2.pipeline/select.instances-from-pks)
+    instance
+    (after-select instance)))
 
 (defmacro define-after-select
   {:style/indent :defn}
   [model [instance-binding] & body]
-  `(tools.simple-out-transform/define-out-transform [:toucan.query-type/select.instances ~model]
-     [instance#]
-     ;; don't do after-select if this select is a result of doing something like insert-returning instances
-     (if (isa? ~'&query-type :toucan2.pipeline/select.instances-from-pks)
-       instance#
-       (let [~instance-binding instance#]
+  `(do
+     (u/maybe-derive ~model ::after-select)
+     (m/defmethod after-select ~model
+       [instance#]
+       (let [~instance-binding (cond-> instance#
+                                 ~'next-method ~'next-method)]
          ~@body))))
 
 (s/fdef define-after-select
