3f276af25f8176afe242a2b349079d84853d5558
John Swanson <john.d.swanson@gmail.com>
2024-02-07 10:56:24 -0800

    add license to POM (#163)
    * add license to POM
    
    also, update license to EPLv1
    
    * update toucan1 compatibility build

--- toucan1/build.clj
+++ toucan1/build.clj
@@ -1,8 +1,11 @@
 (ns build
   (:require [clojure.java.shell :as sh]
             [clojure.string :as str]
-            [org.corfield.build :as bb]))
+            [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
+(def scm-url "git@github.com:camsaul/toucan2.git")
+(def github-url "https://github.com/camsaul/toucan2")
 (def lib 'io.github.camsaul/toucan2-toucan1)
 
 (def major-minor-version (str/trim (slurp "../VERSION.txt")))
@@ -16,14 +19,56 @@
 
 (def version (str major-minor-version \. (commit-number)))
 
-(def options
-  {:lib lib, :version version})
+(def target    "target")
+(def class-dir "target/classes")
+(def jar-file  (format "target/%s-%s.jar" lib version))
 
-(println "Options:" (pr-str options))
+(def sha
+  (or (not-empty (System/getenv "GITHUB_SHA"))
+      (not-empty (-> (sh/sh "git" "rev-parse" "HEAD")
+                     :out
+                     str/trim))))
+
+(def pom-template
+  [[:description "Toucan 1 reimplemented in terms of Toucan 2. Compatibility layer for existing projects using Toucan 1."]
+   [:url github-url]
+   [:licenses
+    [:license
+     [:name "Eclipse Public License"]
+     [:url "http://www.eclipse.org/legal/epl-v10.html"]]]
+   [:developers
+    [:developer
+     [:name "Cam Saul"]]]
+   [:scm
+    [:url github-url]
+    [:connection (str "scm:git:" scm-url)]
+    [:developerConnection (str "scm:git:" scm-url)]
+    [:tag sha]]])
+
+(def options
+  {:lib       lib
+   :version   version
+   :jar-file  jar-file
+   :basis     (b/create-basis {})
+   :class-dir class-dir
+   :target    target
+   :src-dirs  ["src"]
+   :pom-data  pom-template})
+
+(println "Options:" (pr-str (dissoc options :basis)))
 
 (defn build [& _]
-  (bb/clean options)
-  (bb/jar options))
+  (b/delete {:path target})
+  (println "\nWriting pom.xml...")
+  (b/write-pom options)
+  (println "\nCopying source...")
+  (b/copy-dir {:src-dirs   ["src" "resources"]
+               :target-dir class-dir})
+  (printf "\nBuilding %s...\n" jar-file)
+  (b/jar options)
+  (println "Done."))
 
 (defn deploy [& _]
-  (bb/deploy options))
+  (dd/deploy {:installer :remote
+              :artifact  (b/resolve-path jar-file)
+              :pom-file  (b/pom-path (select-keys options [:lib :class-dir]))}))
